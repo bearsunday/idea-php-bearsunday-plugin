88c837299c2fbee2c084f69ec15703c190e9e60d
Shingo Kumagai <shingo4092@gmail.com>
2015-11-12 21:18:36 +0900

    Remove unused imports

--- src/idea/bear/sunday/SettingsForm.java
+++ src/idea/bear/sunday/SettingsForm.java
@@ -3,14 +3,11 @@ package idea.bear.sunday;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
-import idea.bear.sunday.stubs.util.IndexUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class SettingsForm implements Configurable {
 
