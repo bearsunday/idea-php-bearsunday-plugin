546a1efe31eb96ed659d6f177b279ae747f84896
Ryosuke Yoshida <yoshida@excite.jp>
2018-08-13 11:10:20 +0900

    [modify]リソース入力候補の検出場所パターンを修正

--- src/idea/bear/sunday/resource/UriElementPatternHelper.java
+++ src/idea/bear/sunday/resource/UriElementPatternHelper.java
@@ -2,12 +2,46 @@ package idea.bear.sunday.resource;
 
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PlatformPatterns;
+import com.intellij.patterns.StandardPatterns;
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.lang.parser.PhpElementTypes;
+import com.jetbrains.php.lang.patterns.PhpPatterns;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 
 public class UriElementPatternHelper {
 
     public static ElementPattern<PsiElement> getUriDefinition() {
-        return PlatformPatterns.psiElement();
+
+        ElementPattern<PsiElement> pattern =  PlatformPatterns.or(
+            PlatformPatterns.psiElement(PsiElement.class)
+                .withParent(
+                PlatformPatterns.psiElement(StringLiteralExpression.class).withParent(
+                    PlatformPatterns.psiElement(PhpElementTypes.PARAMETER_LIST).withParent(
+                        PlatformPatterns.psiElement(PhpElementTypes.METHOD_REFERENCE).referencing(
+                            PhpPatterns.psiElement().withElementType(
+                                PhpElementTypes.CLASS_METHOD
+                            ).withName(
+                                StandardPatterns.string().oneOf("uri")
+                            )
+                        )
+                    )
+                )
+            ),
+            PlatformPatterns.psiElement(PsiElement.class).withParent(
+                PlatformPatterns.psiElement(StringLiteralExpression.class).withParent(
+                    PlatformPatterns.psiElement(PhpElementTypes.PARAMETER_LIST).withParent(
+                        PlatformPatterns.psiElement(PhpElementTypes.METHOD_REFERENCE).referencing(
+                            PhpPatterns.psiElement().withElementType(
+                                PhpElementTypes.CLASS_METHOD
+                            ).withName(
+                                StandardPatterns.string().oneOf("toInstance")
+                            )
+                        )
+                    )
+                )
+            )
+        );
+        return pattern;
     }
 
 }
