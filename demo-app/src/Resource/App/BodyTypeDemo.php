31ea99b9dce89bb816e5dac49e61d8d313f9a4be
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-06-10 16:08:10 +0900

    Add body type generator demo resource

---
+++ demo-app/src/Resource/App/BodyTypeDemo.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyProject\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+
+/**
+ * Demo target: run "Generate BEAR body type" on this class.
+ */
+final class BodyTypeDemo extends ResourceObject
+{
+    public function onGet(string $name = 'BEAR.Sunday'): static
+    {
+        $this->body = [
+            'id' => 1,
+            'name' => $name,
+            'posts' => [
+                [
+                    'id' => 1,
+                    'title' => 'Hello',
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Body Type',
+                ],
+            ],
+            'meta' => [
+                'active' => true,
+                'score' => 12.5,
+            ],
+        ];
+
+        return $this;
+    }
+
+    public function onPost(): static
+    {
+        $this->body = [
+            'status' => 'created',
+            'id' => 2,
+        ];
+
+        return $this;
+    }
+}
